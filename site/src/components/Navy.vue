ed8488f1422256878c870bf4dc4dd9c67f07f013
Sehaj Gupta <sehaj894@gmail.com>
2018-05-26 13:31:06 -0400

    Theming, History tab, Login page

--- site/src/components/Navy.vue
+++ site/src/components/Navy.vue
@@ -1,13 +1,9 @@
 <template>
-  <v-toolbar>
+  <v-toolbar color="red">
     <v-toolbar-side-icon></v-toolbar-side-icon>
     <v-toolbar-title>alexaFood</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat>Nutrition</v-btn>
-      <v-btn flat>Macros</v-btn>
-      <v-btn flat>History</v-btn>
-      <v-btn flat>Recipes</v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
